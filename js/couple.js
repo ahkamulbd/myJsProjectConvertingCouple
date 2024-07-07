9e3dd71b3799e36930560d94f0ac7b07818e6a6e
ahkamulbd <ahkamulbd@gmail.com>
2024-07-07 21:56:05 +0600

    Inserted the note as comment in couple.js file to get information how the project idea got from

--- js/couple.js
+++ js/couple.js
@@ -1,5 +1,9 @@
 /** 4 men are defined by 4 one (1) and 4 women are defined by 4 zero (0) who were watching movie by sitting beside. Order was started with men and then women in a row. That movie length was 100 minutes. As the movie was going on, all the light were off at 89 minutes and came on at 90 minutes. After on the light, we see the men and women were found in a couple on that row.
  */
+/* 
+How the project idea came from:
+In calculator, if you put 1 button for 4 times and then 0 button for 4 times respectively, the number will be shown as 11110000. Then you have to insert '-' button. Then you will have type 100 for the movie length, and 89 and 90 respectively for light off and on i.e. the number will be 1008990. If you insert '=' button, you will get the result 10101010. 
+*/
 
 function clickToSeeTheOrder() {
     const menCount = '1111';
